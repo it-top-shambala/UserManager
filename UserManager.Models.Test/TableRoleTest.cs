0371272dbf7b1c33afdccc6fa385739cc749e8f3
Starinin Andrey <starininandrey@gmail.com>
2022-07-22 21:07:26 +0300

    Add test proj

---
+++ UserManager.Models.Test/TableRoleTest.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UserManager.Models.Item;
+using UserManager.Models.Table;
+using Xunit;
+
+namespace UserManager.Models.Test;
+
+public class TableRoleTest
+{
+    [Fact]
+    public void GetTable_Test()
+    {
+        var expected = new List<Role>
+        {
+            new Role()
+            {
+                Id = 1,
+                Name = "admin"
+            },
+            new Role()
+            {
+                Id = 2,
+                Name = "user"
+            }
+        };
+        
+        var tableRole = new TableRole();
+        var actual = tableRole.GetTable();
+        
+        Assert.Equal(expected, actual);
+    }
+}
