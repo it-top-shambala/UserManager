7ef7ea2ab4177a245a0a41500bdff1efc1d7c50d
Starinin Andrey <starininandrey@gmail.com>
2022-07-22 21:25:58 +0300

    Add IEquatable<Role>

--- UserManager.Models/Item/Role.cs
+++ UserManager.Models/Item/Role.cs
@@ -1,7 +1,47 @@
 ﻿namespace UserManager.Models.Item;
 
-public class Role
+public class Role : IEquatable<Role>
 {
     public int Id { get; set; }
     public string Name { get; set; }
+
+    public bool Equals(Role? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Id == other.Id && Name == other.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((Role)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name);
+    }
 }
