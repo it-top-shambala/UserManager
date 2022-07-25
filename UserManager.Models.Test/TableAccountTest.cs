705b4e08d7d6553cf6655a8b4c34b718022931e4
Starinin Andrey <starininandrey@gmail.com>
2022-07-25 18:40:58 +0300

    Add test methods

---
+++ UserManager.Models.Test/TableAccountTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UserManager.Models.Table;
+using Xunit;
+using UserManager.Models.Item;
+
+namespace UserManager.Models.Test;
+
+public class TableAccountTest
+{
+    [Fact]
+    public void GetTable_Test()
+    {
+        var expected = new List<Account>
+        {
+            new()
+            {
+                Id = 1,
+                Login = "admin",
+                Password = "123",
+                RoleId = 1,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 2,
+                Login = "user",
+                Password = "123",
+                RoleId = 2,
+                IsActive = true
+            }
+        };
+        var tableAccount = new TableAccount();
+        var actual = tableAccount.GetTable();
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ UserManager.Models.Test/TableUserTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UserManager.Models.Table;
+using Xunit;
+using UserManager.Models.Item;
+
+namespace UserManager.Models.Test;
+
+public class TableUserTest
+{
+    [Fact]
+    public void GetTable_Test()
+    {
+        var expected = new List<User>
+        {
+            new()
+            {
+                Id = 1,
+                FirstName = "anonim",
+                LastName = "anonimus",
+                Email = "add@admin.ru",
+                PhotoUrl = "url"
+            },
+            new()
+            {
+                Id = 2,
+                FirstName = "user",
+                LastName = "anonimus",
+                Email = "user@admin.ru",
+                PhotoUrl = "url"
+            }
+        };
+        var tableUser = new TableUser();
+        var actual = tableUser.GetTable();
+        Assert.Equal(expected, actual);
+    }
+}
