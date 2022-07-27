13a6fe0003d6b578d5313e8c218550f9ae33cad5
Starinin Andrey <starininandrey@gmail.com>
2022-07-27 21:16:35 +0300

    Add components

---
+++ UserManager.App/Components/InputText/InputTextComponent.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace UserManager.App.Components.InputText;
+
+public partial class InputTextComponent : UserControl, INotifyPropertyChanged
+{
+    private string _label;
+    public string LabelContent
+    {
+        get => _label;
+        set
+        {
+            if (value is null) return;
+            if (value == _label) return;
+            _label = value;
+            OnPropertyChanged(nameof(LabelContent));
+        }
+    }
+    
+    private string _input;
+    public string InputText
+    {
+        get => _input;
+        set
+        {
+            if (value is null) return;
+            if (value == _input) return;
+            _input = value;
+            OnPropertyChanged(nameof(InputText));
+        }
+    }
+    
+    public InputTextComponent()
+    {
+        InitializeComponent();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
