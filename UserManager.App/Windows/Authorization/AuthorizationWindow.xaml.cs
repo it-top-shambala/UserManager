926008db7fa87c687b19ce12084179755088bd72
Starinin Andrey <starininandrey@gmail.com>
2022-07-27 19:26:52 +0300

    Update

--- UserManager.App/Windows/Authorization/AuthorizationWindow.xaml.cs
+++ UserManager.App/Windows/Authorization/AuthorizationWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using UserManager.Models.Table;
 
 namespace UserManager.App.Windows.Authorization
 {
@@ -7,6 +8,37 @@ namespace UserManager.App.Windows.Authorization
         public AuthorizationWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonClear_OnClick(object sender, RoutedEventArgs e)
+        {
+            InputLogin.Clear();
+            InputPassword.Clear();
+        }
+
+        private void ButtonLogIn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var login = InputLogin.Text;
+            var password = InputPassword.Password;
+            
+            var accounts = new TableAccount().GetTable();
+
+            var account = accounts.Find(a => a.Login == login && a.Password == password);
+
+            if (account is null)
+            {
+                MessageBox.Show("Неправильно ввели логин и пароль", 
+                    "Ошибка авторизации", 
+                    MessageBoxButton.OK, 
+                    MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Вы успешно авторизовались", 
+                    "Успешная авторизация", 
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
         }
     }
 }
