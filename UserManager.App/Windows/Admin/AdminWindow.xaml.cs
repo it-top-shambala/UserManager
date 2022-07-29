0c6997a3a244b54f0bfd8c50d9cb526d6bb43aa6
Starinin Andrey <starininandrey@gmail.com>
2022-07-29 20:17:12 +0300

    Add AdminWindow

---
+++ UserManager.App/Windows/Admin/AdminWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using UserManager.Models.Item;
+using UserManager.Models.Table;
+
+namespace UserManager.App.Windows.Admin;
+
+public partial class AdminWindow : Window
+{
+    public ObservableCollection<Account> Accounts { get; set; }
+    public AdminWindow()
+    {
+        Accounts = new ObservableCollection<Account>(new TableAccount().GetTable());
+        
+        InitializeComponent();
+    }
+
+    private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        //var account = (Account)((ListView)sender).SelectedItem;
+        var account = (sender as ListView).SelectedItem as Account;
+        var user = new TableUser().GetTable().Find(u => u.Id == account.Id);
+
+        Login.InputText = account.Login;
+        Password.InputText = account.Password;
+        LastName.InputText = user.LastName;
+        FirstName.InputText = user.FirstName;
+        Email.InputText = user.Email;
+    }
+}
+
